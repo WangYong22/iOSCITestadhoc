118afd92816a5df399f9897731e5e2107a9e61bf
WangYong22 <102638006+ WangYong22@users.noreply.github.com>
2025-09-08 20:55:25 +0800

    Initial Commit

---
+++ iOSCITest/iOSCITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCITestApp.swift
+//  iOSCITest
+//
+//  Created by wy on 2025/9/8.
+//
+
+import SwiftUI
+
+@main
+struct iOSCITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSCITestTests/iOSCITestTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCITestTests.swift
+//  iOSCITestTests
+//
+//  Created by wy on 2025/9/8.
+//
+
+import Testing
+@testable import iOSCITest
+
+struct iOSCITestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
