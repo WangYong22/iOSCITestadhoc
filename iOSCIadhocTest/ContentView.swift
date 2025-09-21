c1eee463f1771a59c92fc9f34df1cedbd8eb22ad
WangYong22 <102638006+ WangYong22@users.noreply.github.com>
2025-09-21 13:02:37 +0800

    6

--- iOSCIadhocTest/ContentView.swift
+++ iOSCIadhocTest/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, 5 world!")
+            Text("Hello, 6 world!")
         }
         .padding()
     }
